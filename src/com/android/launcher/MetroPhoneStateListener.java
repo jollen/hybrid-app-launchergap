cb5a1390652cc0631f358cbafeec982f6df73cdb
shonmou365 <shonmou@moko365.com>
2012-12-14 00:23:58 +0800

    Add: MetroPhoneStateListener to capture the phone status of the device.
    if user answer the call, the state is IDLE->RINGING->OFFHOOK->IDLE.
    
    if user does not answer the call, the state is IDLE->RINGING->IDLE.
    
    TODO: using the status to identify missed call status, and using
    CallLog to get how many missed call.

---
+++ src/com/android/launcher/MetroPhoneStateListener.java
@@ -0,0 +1,33 @@
+package com.android.launcher1;
+
+import android.content.Context;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+public class MetroPhoneStateListener extends PhoneStateListener{
+	private static final String TAG = "PhoneStateChanged";
+	Context context;
+	public MetroPhoneStateListener(Context context) {
+		super();
+		this.context = context;
+	}
+	@Override
+	public void onCallStateChanged(int state, String incomingNumber){
+		super.onCallStateChanged(state, incomingNumber);
+		switch (state) {
+			case TelephonyManager.CALL_STATE_IDLE:
+				Toast.makeText(context, "Phone State Idle", Toast.LENGTH_LONG).show();
+				break;
+			case TelephonyManager.CALL_STATE_OFFHOOK:
+				Toast.makeText(context, "Phone State Off hook", Toast.LENGTH_LONG).show();
+				break;
+			case TelephonyManager.CALL_STATE_RINGING:
+				Toast.makeText(context, "Phone State Ringing", Toast.LENGTH_LONG).show();
+				break;
+			default:
+				break;
+		}
+	}
+
+}
---
+++ src/com/android/launcher/PhoneStateBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.android.launcher1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+
+public class PhoneStateBroadcastReceiver extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		TelephonyManager mTelephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+		mTelephonyManager.listen(new MetroPhoneStateListener(context), PhoneStateListener.LISTEN_CALL_STATE);
+	}
+
+}
