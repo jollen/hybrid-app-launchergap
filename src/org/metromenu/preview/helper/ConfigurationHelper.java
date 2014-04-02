f30dbfc882e8963f0faf9c648c970519d2e438a5
jollen <jollen@moko365.com>
2014-04-02 13:07:03 +0800

    Initial commit: branch: preview-1

--- src/org/metromenu/preview/helper/ConfigurationHelper.java
+++ src/org/metromenu/preview/helper/ConfigurationHelper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 Moko365 Inc.
+ * Copyright (C) 2014 Jollen Chen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.metromenu.preview.helper;
 
 public interface ConfigurationHelper {
@@ -6,7 +22,7 @@ public interface ConfigurationHelper {
 	 * MetroMenu goes to edit mode.
 	 * @param edit true when going edit mode
 	 */
-	void setResizableMode(boolean edit);
+	public void setResizableMode(boolean edit); ;
 	
 	boolean getEditMode();
 	
@@ -17,4 +33,6 @@ public interface ConfigurationHelper {
 	void setResortableMode(boolean resort);
 	
 	boolean getResortMode();
+	
+	
 }
