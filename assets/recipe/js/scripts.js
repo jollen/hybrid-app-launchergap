0616e1cb0e56f5b854eb820ccbafcbb50c999a11
Jollen Chen <jollen@moko365.com>
2016-10-30 11:43:57 +0800

    add a new demo: clean recipe (Bootstrap based)

---
+++ assets/recipe/js/scripts.js
@@ -0,0 +1,21 @@
+(function() {
+    "use strict";
+
+    // custom scrollbar
+
+    $("html").niceScroll({styler:"fb",cursorcolor:"#EA6153", cursorwidth: '4', cursorborderradius: '10px', background: '#FFFFFF', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
+
+    $(".scrollbar1").niceScroll({styler:"fb",cursorcolor:"#FF9554", cursorwidth: '4', cursorborderradius: '0',autohidemode: 'false', background: '#FFFFFF', spacebarenabled:false, cursorborder: '0'});
+
+	
+	
+    $(".scrollbar1").getNiceScroll();
+    if ($('body').hasClass('scrollbar1-collapsed')) {
+        $(".scrollbar1").getNiceScroll().hide();
+    }
+
+})(jQuery);
+
+                     
+     
+  
